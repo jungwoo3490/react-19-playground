f5688a0c6dc92223145c72e5ee3f3019ffeb948f
jungwoo3490 <jungwoo3490@naver.com>
2024-12-20 02:40:23 +0900

    chore(backend): add ConfigService import

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,7 +16,7 @@ async function bootstrap() {
     }),
   );
 
-  const configService = app.get('ConfigService');
+  const configService = app.get(ConfigService);
   const PORT = configService.get('SERVER_PORT');
 
   await app.listen(PORT);
